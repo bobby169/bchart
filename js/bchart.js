010cc3caf4df77ea8c8f86c29d21d0d500198ce3
bobby169 <115393304@qq.com>
2013-06-25 19:18:11 +0800

    update20130625

--- js/bchart.js
+++ js/bchart.js
@@ -6,50 +6,60 @@
 (function(win){
     var Bchart=win.Bchart=win.Bchart||{};
     Bchart.Grid=function(){
-        this.elem=null;
-        this.col=15;
-        this.row=50;
+        this.elems=[];
+        this.spanY=35;
+        this.spanX=30;
         this.init();
     }
     Bchart.Grid.prototype={
         init:function(){
-            Bchart.Containner.append(this.render(this.row,this.col));
+            this.draw(this.spanX,this.spanY);
         },
-        render:function(row,col){
-            var tableWidth=(60+1)*col+1;
-            var $table=$("<table style='width:"+tableWidth+"px'></table>");
-            for(var i=0;i<row;i++){
+        draw:function(spanX,spanY){
+            var tableWidth=(40+1)*spanY+1;
+            var $table=$("<table style='width:"+tableWidth+"px'></table>").appendTo(Bchart.Proxy);
+            Bchart.Proxy.css("width",tableWidth);
+            for(var i=0;i<spanX;i++){
                 var $tr=$("<tr></tr>");
-                for(var j=0;j<col;j++){
+                for(var j=0;j<spanY;j++){
                     var $td=$("<td></td>");
                     $tr.append($td);
                 }
                 $table.append($tr);
             }
-
-            this.elem=$table;
-            return this.elem;
+            this.elems.push($table);
         },
         update:function(){
-            this.elem.remove();
-            Bchart.Containner.append(this.render(13,4));
+            this.elems[0].remove();
+            Bchart.Proxy.append(this.draw(13,4));
         },
         zoom:function(zoom){
             var _elem = document.body;
             var sheet = _elem.styleSheet ? _elem.styleSheet : (_elem.sheet || document.styleSheets[document.styleSheets.length - 1]);
             var rules = sheet.cssRules || sheet.rules;
-            //console.info(rules);
+           // console.info(rules);
             for(var i= 0,len=rules.length;i<len;i++){
-                if (rules[i].selectorText.toLowerCase() ==".bchart-container table tr td"){
-                    var height=$(".bchart-container table tr td").height();
+                if (rules[i].selectorText.toLowerCase() ==".bchart-proxy"){
+                   // var height=$(".bchart-container table tr td").height();
+
                     //console.info(rules[i]);
                     //rules[i].style.cssText="border:5px solid red";
                     if(zoom==1){
-                        rules[i].style.height=height*1.25+"px";
+                        rules[i].style.zoom=2;
+                        //if(height>80) return;
+                        //rules[i].style.height=height*2+"px";
+                        //$(".bchart-cell").css({height:$(".bchart-cell").height()*2-9+"px"});*//*
                     }else{
-                        rules[i].style.height=height*(4/5)+"px";
-                    }
+                        rules[i].style.zoom=1;
+                        //if(height<10) return;
+                        //rules[i].style.height=height*(1/2)+"px";
+                        //$(".bchart-cell").css({height:$(".bchart-cell").height()*(1/2)+5+"px"});
 
+                    }
+                    //update Proxy
+                    var h=$(".bchart-container table").height();
+                    Bchart.Proxy.css({height:h});
+                    break;
                 }
             }
 
@@ -58,22 +68,44 @@
 
     Bchart.Cell=function(opt){
         this.height=opt.height;
+        this.elems=[];
+        this.spanY=35;
+        this.spanX=30;
+        this.init();
         //this.color=opt.color;
     }
 
     Bchart.Cell.prototype={
-        draw:function(){
-
+        init:function(){
+            this.draw(this.spanX,this.spanY);
         },
-        render:function(){
-            var td=$(".bchart-container table tr").eq(0).find("td");
-            for(var i=0;i<td.length;i++){
-                var $cell=$("<div class='cell'></div>").css({"height":100,width:10,left:i*61,background:"blue"}).appendTo(Bchart.Containner);
-
-
+        draw:function(spanX,spanY){
+            for(var j=0;j<spanY;j++){
+                var $cell=$("<div class='bchart-cell'></div>").css({"height":Bchart.Util.randomInt(20,200),width:10,left:j*41,background:"#99dcf9"}).appendTo(Bchart.Proxy);
+                this.elems.push($cell);
             }
         }
 
+
+    }
+
+    Bchart.Coordinate=function(type){
+         this.type=type;
+    }
+    Bchart.Coordinate.prototype={
+        init:function(){
+
+        },
+        draw:function(spanX,spanY){
+            for(var i=0;i<spanX;i++){
+                var $coordX=$("<span class='bchart-coordinateX'></span>").appendTo(Bchart.Proxy);
+
+            }
+            for(var j=0;j<spanY;j++){
+                var $coordY=$("<span class='bchart-coordinateY'></span>").appendTo(Bchart.Proxy);
+
+            }
+        }
     }
 
     Bchart.Event={
@@ -129,8 +161,7 @@
                 var spanY = (pos.y - lastMouseY);
                 var w= currentElement.offsetWidth-Bchart.Containner[0].offsetWidth;
                 var h= currentElement.offsetHeight-Bchart.Containner[0].offsetHeight;
-
-                if(lastElemLeft + spanX>0){
+                /*if(lastElemLeft + spanX>0){
                     $(currentElement).css({"left":0});
                     return;
                 }else if(lastElemLeft + spanX<(-w)){
@@ -143,7 +174,7 @@
                 }else if(lastElemTop + spanY<(-h)){
                     $(currentElement).css({"top":(-h)+"px"});
                     return;
-                }
+                } */
                 if(def.direction=="top"){
                     $(currentElement).css("top", (lastElemTop + spanY));
                 }else if(def.direction=="left"){
@@ -174,26 +205,28 @@
                 }
             });
             (function(allowBubbling ){
-                var me=target[0];
-                if(undefined == me.id || !me.id.length) me.id = "drag"+(new Date().getTime());
-                bubblings[me.id] = allowBubbling ? true : false;
-                dragStatus[me.id] = "on";
-                target.css("cursor", "move");
-                target.mousedown(function(e){
-                    if((dragStatus[me.id] == "off") || (dragStatus[me.id] == "handler" && !holdingHandler))
-                        return bubblings[me.id];
-                    target.css("position", "absolute");
-                   // target.css("z-index", parseInt( new Date().getTime()/1000 ));
-                    isMouseDown = true;
-                    currentElement = me;
-                    var pos = getMousePosition(e);
-                    lastMouseX = pos.x;
-                    lastMouseY = pos.y;
-                    lastElemTop = me.offsetTop;
-                    lastElemLeft = me.offsetLeft;
-                    updatePosition(e);
-                    return bubblings[me.id];
-                });
+                target.each(function(){
+                    if(undefined == this.id || !this.id.length) this.id = "drag"+(new Date().getTime());
+                    bubblings[this.id] = allowBubbling ? true : false;
+                    dragStatus[this.id] = "on";
+                    $(this).css("cursor", "move");
+                    $(this).mousedown(function(e){
+                        if((dragStatus[this.id] == "off") || (dragStatus[this.id] == "handler" && !holdingHandler))
+                            return bubblings[this.id];
+                        $(this).css("position", "absolute");
+                        $(this).css("z-index", parseInt( new Date().getTime()/1000 ));
+                        isMouseDown = true;
+                        currentElement = this;
+                        var pos = $.getMousePosition(e);
+                        lastMouseX = pos.x;
+                        lastMouseY = pos.y;
+                        lastElemTop = this.offsetTop;
+                        lastElemLeft = this.offsetLeft;
+                        $.updatePosition(e);
+                        return bubblings[this.id];
+                    });
+                })
+
             })();
            /* $.fn.ondrag = function(callback){
                 return this.each(function(){
@@ -205,6 +238,11 @@
         }
 
     }
+    Bchart.Util={
+        randomInt:function(min,max){
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+    }
     Bchart.Init=function(containner){
         Bchart.Containner=containner||$("<div class='bchart-containner'></div>").appendTo($(document.body));
         Bchart.Containner.addClass("bchart-container");
@@ -213,11 +251,14 @@
         //grid.update();
         grid.zoom();
         Bchart.Event.scroll(grid);
-        Bchart.Event.drag($("table"),{
-           // direction:"left"
-        })
+
         var cell=new Bchart.Cell(30);
-        cell.render();
-      //$("table").easydrag()
+        //cell.render();
+        //console.info(cell.elems[0])
+        //cell.elems[5].css({"background":"red"})
+        Bchart.Event.drag($(".bchart-proxy"),{
+            // direction:"left"
+        })
+    //  $(".bchart-cell").easydrag()
     }
 })(window)
