f630f0f079230faa04a5d05e8fca9165c9927090
bobby169 <115393304@qq.com>
2013-07-01 19:21:53 +0800

    update 2013/07/01

--- js/bchart.js
+++ js/bchart.js
@@ -102,13 +102,18 @@
 
             $twobarSrc=$("<div class='bchart-twobar-src'></div>").css({"left":"82px"}).appendTo(Bchart.Proxy);
             $twobarDist=$("<div class='bchart-twobar-dist'></div>").css({"left":"205px","height":"80px"}).appendTo(Bchart.Proxy);
-
+            var me=this;
+            var cood=me.getCood($twobarSrc,$twobarDist);
+            new Bchart.Line(0,cood.x1,cood.y1,cood.x2,cood.y2);
           //  console.info($twobarDist)
 
             Bchart.Event.resize($twobarSrc,$twobarDist,function(e,currentElement,change){
                 // console.info(change)
                 var h=$twobarSrc.height()+change*10;
                 $(".bchart-twobar-dist").css({height:h});
+                var cood=me.getCood($twobarSrc,$twobarDist);
+                new Bchart.Line(0,cood.x1,cood.y1,cood.x2,cood.y2);
+                //console.info(cood)
                 return true;
             });
 
@@ -117,11 +122,15 @@
             },function(e,currentElement,curDate,moveDate){
                 var h=$twobarDist.height();
                 var change=curDate+moveDate;
-                console.info(change)
+                //console.info(change)
 
                 h=  $(".bchart-twobar-src").height()+change*10-2*10;
                 //console.info(h)
                 $twobarDist.css({height:h+"px"}).attr("title",h);
+
+                var cood=me.getCood($twobarSrc,$twobarDist);
+                new Bchart.Line(0,cood.x1,cood.y1,cood.x2,cood.y2);
+                //console.info(cood)
                 //i++;
                 //console.info(change)
                 //conm,sole.info(curDate,moveDate)
@@ -134,6 +143,19 @@
 
 
 
+        },
+        getCood:function($obj1,$obj2){
+            var parent=$obj1.parent();
+            var x1=$obj1[0].offsetLeft;
+            var x2=$obj2[0].offsetLeft;
+            var y1=parent.height()-$obj1.height();
+            var y2=parent.height()-$obj2.height();
+            return{
+                x1:x1,
+                y1:y1,
+                x2:x2,
+                y2:y2
+            }
         }
     }
 
@@ -154,7 +176,7 @@
             if( !objectHandle )
             {
                 //document.body.innerHTML += "<img id='line"+ lineIndex +"' class='line' />"
-                Bchart.Proxybox.append("<img id='line"+ lineIndex +"' class='line' />");
+                Bchart.Proxy.append("<img id='line"+ lineIndex +"' class='line' />");
                 objectHandle = document.getElementById( "line"+ lineIndex )
             }
 
@@ -172,10 +194,10 @@
                 tmp			= Math.min( boxWidth, boxHeight, 256 ),
                 lineIndex	= (Bx-Ax)*(By-Ay)<0?0:1
 
-            console.info(tmp,lineIndex)
+           // console.info(tmp,lineIndex)
             while( tmp=tmp>>1 ){
                 lineIndex+=2
-               console.warn(tmp,lineIndex)
+                //console.warn(tmp,lineIndex)
             }
 
             lineObjectHandle.src = preloadedImages[lineIndex].src
@@ -440,7 +462,7 @@
 
         new Bchart.Twobar();
 
-        new Bchart.Line(1, 0,  0, 200, 50);
+        //new Bchart.Line(1, 82,  580, 605, 550);
         //cell.render();
         //console.info(cell.elems[0])
         //cell.elems[5].css({"background":"red"})
